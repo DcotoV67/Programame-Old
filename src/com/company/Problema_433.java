d35b932ecf0c3433218e41f6dcc30860b94bf913
Dcotov67 <dcotov67@elpuig.xeill.net>
2018-10-30 16:43:06 +0100

    Ejercicio 433

---
+++ src/com/company/Problema_433.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Problema_433 {
+    public static void main(String[] args) {
+
+        int total_uvas = 416541641;
+
+        Scanner in = new Scanner (System.in);
+
+        while(total_uvas != 0) {
+            total_uvas = in.nextInt();
+            int uvas = 0;
+            int nivel = 0;
+
+            while (total_uvas > uvas) {
+
+                nivel++;
+                uvas = nivel + uvas;
+            }
+            if(nivel!=0) {
+                System.out.println(nivel);
+            }
+        }
+
+        while(total_uvas != 0) {
+            total_uvas = in.nextInt();
+            int uvas = 0;
+            int nivel = 0;
+
+            while (total_uvas > uvas) {
+
+                nivel++;
+                uvas = nivel + uvas;
+            }
+            if(nivel!=0) {
+                System.out.println(nivel);
+            }
+        }
+    }
+}
